341e9365950f6111774c41c1043a7c9213a79af5
wesblake@proton.me <wesblake@proton.me>
2026-04-23 08:12:46 -0700

    Change: force fill wallpaper Change: cleaned up code to wrap lines.

--- wallpaper_changer.py
+++ wallpaper_changer.py
@@ -1,4 +1,5 @@
 import ctypes
+import winreg
 import os
 from pathlib import Path
 
@@ -7,8 +8,32 @@ def set_wallpaper(image_path: str) -> None:
     SPIF_UPDATEINIFILE = 0x01
     SPIF_SENDWININICHANGE = 0x02
 
+    with winreg.OpenKey(
+        winreg.HKEY_CURRENT_USER,
+        r"Control Panel\Desktop",
+        0,
+        winreg.KEY_SET_VALUE
+    ) as key:
+        winreg.SetValueEx(
+            key,
+            "WallpaperStyle",
+            0,
+            winreg.REG_SZ,
+            "3"  # 0: Center, 1: Tile, 2: Stretch, 3: Fit, 4: Fill
+        )
+        winreg.SetValueEx(
+            key,
+            "TileWallpaper",
+            0,
+            winreg.REG_SZ,
+            "0"  # 0: No tiling, 1: Tile
+        )
+
     result = ctypes.windll.user32.SystemParametersInfoW(
-        SPI_SETDESKWALLPAPER, 0, image_path, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE
+        SPI_SETDESKWALLPAPER,
+        0,
+        image_path,
+        SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE
     )
 
     if not result:
@@ -16,13 +41,20 @@ def set_wallpaper(image_path: str) -> None:
 
 def rand_wallpaper() -> str:
     PICTURES_DIR = Path.home() / 'Pictures'
-    wallpapers = list(PICTURES_DIR.glob('*.jpg')) + list(PICTURES_DIR.glob('*.jpeg')) + list(PICTURES_DIR.glob('*.png'))
+    wallpapers = list(
+        PICTURES_DIR.glob('*.jpg')) + \
+        list(PICTURES_DIR.glob('*.jpeg')) + \
+        list(PICTURES_DIR.glob('*.png')
+    )
     import random
     return str(random.choice(wallpapers))
 
 def get_wallpaper() -> str:
     PICTURES_DIR = Path.home() / 'Pictures'
-    wallpapers = list(PICTURES_DIR.glob('*.jpg')) + list(PICTURES_DIR.glob('*.jpeg')) + list(PICTURES_DIR.glob('*.png'))
+    wallpapers = list(
+        PICTURES_DIR.glob('*.jpg')) + \
+        list(PICTURES_DIR.glob('*.jpeg')) + \
+        list(PICTURES_DIR.glob('*.png'))
     while True:
         os.system('cls' if os.name == 'nt' else 'clear')
         for i, v in enumerate(wallpapers):
@@ -42,6 +74,4 @@ def get_wallpaper() -> str:
     return str(wallpapers[index % len(wallpapers)])
 
 if __name__ == "__main__":
-    #path_o_interest = get_wallpaper()
-    #set_wallpaper(path_o_interest)
     set_wallpaper(rand_wallpaper())
